ae5b55cc2cd844fee0ec99960522cc290d7e2fb0
Poop-Maxiiiiiiiiiiiiiiiiiiiiiiiiiiiii <166578919+Poop-Maxiiiiiiiiiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-05-10 11:29:57 +0800

    Update chess.js

--- chess.js
+++ chess.js
@@ -1,1 +1,34 @@
-
+let ifWhite = true;
+white = 500;
+black = 500;
+function start()
+{
+  setInterval(function()
+              {
+                if(ifWhite)
+                {
+                  white -= 1;
+                }
+                else
+                {
+                  black -= 1;
+                }
+                if(white != 0 && black != 0)
+                {
+                  update();
+                }
+              },1000);
+}
+function white()
+{
+  ifWhite = true;
+}
+function black()
+{
+  ifWhite = false;
+}
+function update()
+{
+  document.getElementById("white").value = `${Math.floor(white/60)}:${white % 60}`;
+  socument.getElementById("black").value = `${Math.floor(black/60)}:${black % 60}`;
+}
