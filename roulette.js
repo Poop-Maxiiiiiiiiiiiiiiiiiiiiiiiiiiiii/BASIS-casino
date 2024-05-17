9d99097b40e384e44d34e48c4b35cbbfe144970a
Poop-Maxiiiiiiiiiiiiiiiiiiiiiiiiiiiii <166578919+Poop-Maxiiiiiiiiiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-05-17 11:24:34 +0800

    Update roulette.js

--- roulette.js
+++ roulette.js
@@ -1,6 +1,3 @@
-
-//window.location.href("https://www.bit.ly/the-power-of-link")
-
 // Get the wheel and spin button elements from the DOM
 const wheel = document.getElementById("wheel");
 const spinBtn = document.getElementById("spin-button");
@@ -8,12 +5,12 @@ const spinBtn = document.getElementById("spin-button");
 // Object that stores values of minimum and maximum angle for a value
 const rotationValues = 
 [
-    { minDegree: 0, maxDegree: 60, value: "BRUH" },
-    { minDegree: 61, maxDegree: 120, value: "GET RICKROLLED" },
-    { minDegree: 121, maxDegree: 180, value: "5 DBUCK" },
-    { minDegree: 181, maxDegree: 240, value: "LOL" },
-    { minDegree: 241, maxDegree: 300, value: "IDK" },
-    { minDegree: 301, maxDegree: 360, value: "LMAO" },
+    { minDegree: 0, maxDegree: 60, value: 1 },
+    { minDegree: 61, maxDegree: 120, value: 2 },
+    { minDegree: 121, maxDegree: 180, value: 3 },
+    { minDegree: 181, maxDegree: 240, value: 4 },
+    { minDegree: 241, maxDegree: 300, value: 5 },
+    { minDegree: 301, maxDegree: 360, value: 6 },
 ];
 
 // Size of each piece (all pieces are of equal size)
@@ -22,12 +19,12 @@ const data = [1, 1, 1, 1, 1, 1];
 // Background color for each piece
 let pieColors = 
 [
-    "#abcdef", // Dark blue
-    "#aaddad", // Lighter blue
-    "#abcdef", // Dark blue
-    "#aaddad",
-    "#abcdef", // Dark blue
-    "#aaddad"
+    "#325AE4", // Dark blue
+    "#3F6DF0", // Lighter blue
+    "#325AE4", // Dark blue
+    "#3F6DF0", // Lighter blue
+    "#325AE4", // Dark blue
+    "#3F6DF0"  // Lighter blue
 ];
 
 // Create the chart
