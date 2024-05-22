041e54eedaa3a70134a0a4f7f550ad55a0a3a00f
Poop-Maxiiiiiiiiiiiiiiiiiiiiiiiiiiiii <166578919+Poop-Maxiiiiiiiiiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-05-22 11:20:23 +0800

    Update Slot.js

--- Slot.js
+++ Slot.js
@@ -1,15 +1,17 @@
 
-  
+
 function spin()
 {
   let symbols = ["🍕","🍟","🥘","🧆","🥖","🍔","🧀","🗿"];
   let slot1 = symbols[Math.floor(Math.random() * symbols.length)];
   let slot2 = symbols[Math.floor(Math.random() * symbols.length)];
   let slot3 = symbols[Math.floor(Math.random() * symbols.length)];
-  let return = document.getElementByI("reutrn");
+  document.getElementById('slot1').innerHTML = slot1;
+  document.getElementById('slot2').innerHTML = slot2;
+  document.getElementById('slot3').innerHTML = slot3;
+  let returny = document.getElementById("reutrn");
   if (slot1 == slot2 && slot2 == slot3)
   {
-    return.innerHTML = `Wow! you won! you spun ${slot1}|${slot1}|${slot1}|`; 
+    returny.innerHTML = `Wow! you won! you spun ${slot1}|${slot1}|${slot1}|`; 
   }
 }
-
